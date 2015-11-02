96a81c3cc1aa4bf0ef01ef643f9f780eb7e2b9f4
Carlos Salazar <carlos_3598@hotmai.com>
2015-11-02 13:24:18 -0600

    Prueba

--- prueba/urls.py
+++ prueba/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'Donations.views.home', name="home"),
     url(r'^admin/', include(admin.site.urls)),
 ]
