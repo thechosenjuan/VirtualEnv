6b2a41c271418d27ec89a669bedd6de6ad4bd748
Juan Flores <juanm.flores127@gmail.com>
2015-11-19 11:41:21 -0600

    base finally merged

--- prueba/settings.py
+++ prueba/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'prueba.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "Donations/templates/Donations")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
